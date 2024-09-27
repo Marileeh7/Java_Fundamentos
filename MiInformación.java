9594616e348730aee2359fc58e83b1be5b1064c9
Marileeh7 <marileeh7@gmail,com>
2024-09-27 13:55:18 -0400

    Primera versión del proyecto de Java

---
+++ MiInformación.java
@@ -0,0 +1,8 @@
+public class MiInformación {
+    public static void main(String[] args) {
+        System.out.println("Hola, mi nombre es MARIA LETICIA RODAS.");
+        System.out.println("Tengo 30 años de edad.");
+        System.out.println("Soy de [Tu País] y vivo en la ciudad de [Tu Ciudad].");
+        System.out.println("Me gusta mucho [Pasatiempo1] y [Pasatiempo2].");
+    }
+}
