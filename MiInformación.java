85ed1f4e537af9f1534cf8970255fe06e3db5504
Marileeh7 <marileeh7@gmail,com>
2024-09-27 20:39:19 -0400

    Cambio 1

--- MiInformación.java
+++ MiInformación.java
@@ -2,7 +2,8 @@ public class MiInformación {
     public static void main(String[] args) {
         System.out.println("Hola, mi nombre es MARIA LETICIA RODAS.");
         System.out.println("Tengo 30 años de edad.");
-        System.out.println("Soy de [Tu País] y vivo en la ciudad de [Tu Ciudad].");
-        System.out.println("Me gusta mucho [Pasatiempo1] y [Pasatiempo2].");
+        System.out.println("Soy de Paraguay y vivo en la ciudad de Encarnación.");
+        System.out.println("Me gusta mucho viajar y leer.");
     }
 }
+
